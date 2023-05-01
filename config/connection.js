9afc5c8221262c4f3aa2ff5478a27f607d8c5b2b
Joaquin Salazar <joaqsala@gmail.com>
2023-04-30 21:28:48 -0500

    corrected config file and import/export

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
-// require('dotenv').config();
+require('dotenv').config();
 
-// const config = {
-//     host: 'localhost',
-//   // MySQL username,
-//     user: process.env.DB_USER,
-//   // MySQL password
-//     password: process.env.DB_PASSWORD,
-//     database: 'employee_db'
-// }
+const config = {
+    host: 'localhost',
+  // MySQL username,
+    user: process.env.DB_USER,
+  // MySQL password
+    password: process.env.DB_PASSWORD,
+    database: 'employee_db',
 
-// module.exports = config
+    isServer: true,
+}
+
+module.exports = { config }
